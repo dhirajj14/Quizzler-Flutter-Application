f2d7a8f763249f9dafd14bc4c78a74544051adc2
dhirajj14 <djain14@hawk.iit.edu>
2020-07-09 15:06:50 -0500

    add encapsulation to code

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -69,19 +67,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
                 if (correctAnswer == true) {
                   print("right");
                 } else {
                   print("wrong");
                 }
                 setState(() {
-                  if (questionNumber == quizBrain.questionBank.length - 1) {
-                    questionNumber = 0;
-                  } else {
-                    questionNumber++;
-                  }
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -101,19 +94,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
                 if (correctAnswer == false) {
                   print("right");
                 } else {
                   print("wrong");
                 }
                 setState(() {
-                  if (questionNumber == quizBrain.questionBank.length - 1) {
-                    questionNumber = 0;
-                  } else {
-                    questionNumber++;
-                  }
+                  quizBrain.nextQuestion();
                 });
               },
             ),
